beeb7d66850b9add6210afcc12c11fdc9bba3ecd
liu.shuai <769741679@qq.com>
2020-11-15 11:43:01 +0800

    【M】提交包和mp配置文件

---
+++ sharding-inline-yml/src/main/java/cn/cestc/inline/config/MybatisPlusConfig.java
@@ -0,0 +1,17 @@
+package cn.cestc.inline.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(basePackages = "cn.cestc.biz.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setLimit(-1);
+        return paginationInterceptor;
+    }
+}
