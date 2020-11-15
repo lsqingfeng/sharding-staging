7bd99de9a03c5c5336946ea3cb2915cabbffc7d0
liu.shuai <769741679@qq.com>
2020-11-15 12:01:08 +0800

    提交pom, controller

---
+++ sharding-standard-java/src/main/java/cn/cestc/standard/controller/TestController.java
@@ -0,0 +1,18 @@
+package cn.cestc.standard.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @className: TestController
+ * @description:
+ * @author: sh.Liu
+ * @date: 2020-11-15 11:54
+ */
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+}
