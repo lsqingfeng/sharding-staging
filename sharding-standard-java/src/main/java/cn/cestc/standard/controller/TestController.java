0cfcfbffccfc315cde43f460c41ab34b0c8cf893
liu.shuai <769741679@qq.com>
2020-11-19 12:14:50 +0800

    aaa

--- sharding-standard-java/src/main/java/cn/cestc/standard/controller/TestController.java
+++ sharding-standard-java/src/main/java/cn/cestc/standard/controller/TestController.java
@@ -102,7 +102,7 @@ public class TestController {
     // UnsupportedOperationException
     @RequestMapping("/listOrder3")
     public Object testListOrder3(){
-        List<Order> list = orderService.list(new LambdaQueryWrapper<Order>().between(Order::getAddressId, 100L, 127L).orderByAsc(Order::getUserId));
+        List<Order> list = orderService.list(new LambdaQueryWrapper<Order>().between(Order::getUserId, 70L, 90L).orderByAsc(Order::getUserId));
         return list;
     }
 }
